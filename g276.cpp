907ccadc8756082992ffb1c0fcc5e138b7eca2d2
Youtong0826 <jimmy2007826@gmail.com>
2024-01-01 15:11:01 +0800

    update

--- g276.cpp
+++ g276.cpp
@@ -1,11 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, m, k, mp[105][105], val[505][5], dn = 0;
+struct P{
+    int r;
+    int c;
+    int s;
+    int t;
+    int f;
+};
+
+int n, m, k, mp[105][105];
+set<pair<int, int>> bombs;
+P ps[505];
+
 bool in(int x, int y){
     return x >= 0 && x < n && y >= 0 && y < m;
 }
+
 int main(){
+    ios::sync_with_stdio(0); 
+    cin.tie(0); cout.tie(0);
+
     cin >> n >> m >> k;
     for (int i = 0; i < n; i++){
         for (int j = 0; j < m; j++)
@@ -13,42 +28,32 @@ int main(){
     }
 
     for (int i = 0; i < k; i++){
-        val[i][4] = 1;
-        cin >> val[i][0] >> val[i][1] >> val[i][2] >> val[i][3];// r c s t
+        ps[i].f = 1;
+        cin >> ps[i].r >> ps[i].c >> ps[i].s >> ps[i].t; 
     }
 
-    int t = 5;
-    while (t--){
-        if(dn == k)
-            break;
-
+    int t = k;
+    while (t){
+        bombs.clear();
         for (int i = 0; i < k; i++){
-            if (!val[i][4]){
-                dn++;
+            if (!ps[i].f)
                 continue;
-            }
-
-            mp[val[i][0]][val[i][1]] = 2;
-            val[i][0] = val[i][0] + val[i][2];
-            val[i][1] += val[i][3];
+            mp[ps[i].r][ps[i].c] = 2; //bomb;
         }
-        
         for (int i = 0; i < k; i++){
-            if(mp[val[i][0]][val[i][1]] == 2 || !in(val[i][0], val[i][1]))
-                val[i][4] = 0;
-            
-            else
-                mp[val[i][0]][val[i][1]] = 1;
-        }
-        
-        cout << "\n\n";
-        for (int i = 0; i < n; i++){
-            for (int j = 0; j < m; j++){
-                cout << mp[i][j] << ' ';
+            if (!ps[i].f)
+                continue;
+            ps[i].r += ps[i].s;
+            ps[i].c += ps[i].t;
+            if (mp[ps[i].r][ps[i].c] == 2 || !in(ps[i].r, ps[i].c)){
+                t--;
+                ps[i].f = 0;
+                bombs.insert({ps[i].r, ps[i].c});
             }
-            cout << "\n";
         }
-        
+
+        for (auto b: bombs)
+            mp[b.first][b.second] = 0;
     }
     
     int ans = 0;
