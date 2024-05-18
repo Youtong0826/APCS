ac9fee1b4063b539dc1e924f134e7bb7d14bc923
Youtong0826 <jimmy2007826@gmail.com>
2024-05-19 00:29:35 +0800

    removed file

--- e313.py
+++
@@ -1,3 +0,0 @@
-from sys import stdin
-t = int(stdin.readline())
-print(min(stdin.readlines(), key=lambda x: (len(set(x)), x)))
--- i399.py
+++
@@ -1,2 +0,0 @@
-n = sorted(set(input().split()), key=int, reverse=True)
-print(4-len(n), " ".join(n))
