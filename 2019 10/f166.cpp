28367d01d85e3869f4ec139ada78b7c54502bf2a
Youtong0826 <jimmy2007826@gmail.com>
2024-05-19 00:31:03 +0800

    2019 10

---
+++ 2019 10/f166.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, p, L, R;
+vector<int> s;
+
+int main(){
+    cin >> n >> p >> L >> R;
+    s.assign(n, 0);
+    for (int i = 0; i <n; i++)
+        cin >> s[i];
+
+}
