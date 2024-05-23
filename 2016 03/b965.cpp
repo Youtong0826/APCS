b82d86c83d9f490b274a837ff4f0685c7f73f105
Youtong0826 <jimmy2007826@gmail.com>
2024-05-24 02:55:23 +0800

    b965

--- 2016 03/b965.cpp
+++ 2016 03/b965.cpp
@@ -20,8 +20,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int arr[10][10], r, c, m;
-vector<int> ops;
+int arr[10][10], r, c, m, ops;
 
 void flip_back(){
     for (int i = 0; i < r/2; i++)
@@ -37,35 +36,32 @@ void rotate_back(){
 
     swap(r, c);
     for (int i = 0; i < r; i++)
-        for(int j = 0; j < c; j++)
+        for (int j = 0; j < c; j++)
             arr[i][j] = tmp[j][r-i-1];
 }
 
 int main(){
-    while(cin >> r >> c >> m){
-        for (int i = 0; i < r; i++)
-            for (int j = 0; j < c; j++)
-                cin >> arr[i][j];
-
-        ops.assign(m, 0);
-        for (int i = 0; i < m; i++)
-            cin >> ops[i];
-
-        while (m--){
-            if (ops.back())
-                flip_back();
-
-            else
-                rotate_back();
-
-            ops.pop_back();
+    cin >> r >> c >> m;
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            cin >> arr[i][j];
         }
+    }
+    
+    while (m--) {
+        cin >> ops;
+        if (ops)
+            flip_back();
 
-        cout << r << ' ' << c << '\n';
-        for (int i = 0; i < r; i++){
-            for (int j = 0; j < c; j++)
-                cout << arr[i][j] << ((j+1 == c)? "":" ");
-            cout << '\n';
-        }
+        else
+            rotate_back();   
+    }
+
+    cout << r << ' ' << c << '\n';
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) 
+              cout << arr[i][j] << ((j+1 == c)? "":" ");
+        
+        cout << '\n';
     }
 }
