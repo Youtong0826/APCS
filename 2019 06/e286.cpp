253163c20aed0a5363117000445ef49b9032f732
Youtong0826 <jimmy2007826@gmail.com>
2024-07-13 08:34:48 +0800

    e286

--- 2019 06/e286.cpp
+++ 2019 06/e286.cpp
@@ -1,34 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int sum(int arr[], int length){
-    int n = 0;
-    for (int i = 0; i < length; i++)
-        n += arr[i];
-    return n;
-}
-
 int main(){
-    int a[4], b[4], c = 0;
-    for (int i = 0; i < 2; i++){
-        for (int j = 0; j < 4; j++)
-            cin >> a[j];
-        
-        for (int j = 0; j < 4; j++)
-            cin >> b[j];
+    int a = 0, b = 0, x, y, t;
+    for (int i = 0; i < 2; i++) {
+        x = 0, y = 0;
 
-        int x = sum(a, 4), y = sum(b, 4);
+        for (int j = 0; j < 4; j++) {
+            cin >> t;
+            x += t;
+        }
 
-        cout << x << ':' << y << '\n';
-        c += (x > y)? 1 : (x == y)? 0 : -1;
+        for (int j = 0; j < 4; j++) {
+            cin >> t;
+            y += t;
+        }
+
+        if (x > y) 
+            a++;
+
+        else if (x < y) 
+            b++;
+
+        cout << x << ':' << y << '\n'; 
     }
-
-    if (c > 0)
+    
+    if (a > b)
         cout << "Win";
-        
-    else if (c < 0)
+
+    else if (a < b)
         cout << "Lose";
 
     else
         cout << "Tie";
 }
+ 
