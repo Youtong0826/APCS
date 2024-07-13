fc6390f9c7816b51d7d7ef72d2b8c4c6897ce346
Youtong0826 <jimmy2007826@gmail.com>
2024-07-14 01:08:06 +0800

    m371

--- 2023 10/m371.cpp
+++ 2023 10/m371.cpp
@@ -2,9 +2,8 @@
 using namespace std;
 
 int mp[25][40];
+
 int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
     int n, m, sum = 0;
     cin >> n >> m;
     for (int i = 0; i < n; i++){
@@ -13,37 +12,33 @@ int main(){
         }
     }
 
-    for (int t = 0; t < n*m; t++){
+    for (int t = 0; t < n*m/2+1; t++){ // 最多只會做 n*m/2 次
         for (int i = 0; i < n; i++){
             for (int j = 0; j < m; j++){
-                if (mp[i][j] < 1)
+                if (mp[i][j] == -1)
                     continue;
 
-                for (int k = j+1; k < m; k++){
+                for (int k = j+1; k < m; k++){ // 檢查橫排
                     if(mp[i][k] == -1)
                         continue;
 
                     if (mp[i][j] == mp[i][k]){ 
                         sum += mp[i][k];
                         mp[i][k] = mp[i][j] = -1;
-                        break;
                     }
-
-                    else
-                        break;
+                    break;
                 }
-                for (int k = i+1; k < n; k++){
+
+
+                for (int k = i+1; k < n; k++){ // 檢查直排
                     if(mp[k][j] == -1)
                         continue;
 
                     if (mp[i][j] == mp[k][j]){ 
                         sum += mp[k][j];
                         mp[k][j] = mp[i][j] = -1;
-                        break;
                     }
-
-                    else
-                        break;
+                    break;
                 }
             }
         }
