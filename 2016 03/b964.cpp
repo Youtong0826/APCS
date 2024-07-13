a2e6dc84e51a538f6181e31cbb2966917c41331d
Youtong0826 <jimmy2007826@gmail.com>
2024-07-13 08:33:46 +0800

    update b964

--- 2016 03/b964.cpp
+++ 2016 03/b964.cpp
@@ -1,32 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 int m[21];
-
 int main(){
     int n, minX = INT_MAX, maxY = INT_MIN; 
-    // 宣告變數，minX 意旨最低的及格分數，minY 意旨最高的不及格分數
-    cin >> n; // 讀取學生人數
+    cin >> n;
     for (int i = 0; i < n; i++){
         cin >> m[i];
-        if (m[i] >= 60) // 如果大於等於 60
-            minX = min(minX, m[i]); // 取較小值
+        if (m[i] >= 60) 
+            minX = min(minX, m[i]); 
         
-        else // 否則
-            maxY = max(maxY, m[i]); // 取較大值
+        else 
+            maxY = max(maxY, m[i]);
     }
 
     sort(m, m+n); // 對 m 由小到大排序
-    for (int i = 0; i < n; i++) // 輸出分數
+    for (int i = 0; i < n; i++) 
         cout << m[i] << ' ';
     
     cout << '\n';
-    if (minX == INT_MAX) // 如果沒有及格的最小值
+    if (minX == INT_MAX) 
         cout << maxY << '\n' << "worst case";
 
-    else if (maxY == INT_MIN) // 如果沒有不及格的最大值
+    else if (maxY == INT_MIN) 
         cout << "best case" << '\n' << minX;
     
-    else // 否則輸出及格最小值和不及格最大值
+    else 
         cout << maxY << '\n' << minX << '\n';
 }
