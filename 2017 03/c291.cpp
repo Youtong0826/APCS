670c4c280dfb38d7ade37e6a16a7056ed71b3da9
Youtong0826 <jimmy2007826@gmail.com>
2024-07-14 15:17:08 +0800

    c291

--- 2017 03/c291.cpp
+++ 2017 03/c291.cpp
@@ -1,32 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, f[500000], a=0;
-bool been[500000]={};
-
-void dfs(int id){
-    if (been[f[id]])
-        return;
-    else{
-        been[f[id]] = 1;
-        dfs(f[id]);
-    }
-}
+const int maxn = 5e5+1;
+int n, f[maxn], cnt = 0;
+bool vis[maxn];
 
 int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0), cout.tie();
     cin >> n;
-
     for (int i = 0; i < n; i++)
         cin >> f[i];
 
-    for (int i = 0; i < n; i++)
-        if(!been[f[i]]){
-            a++;
-            been[i]=1;
-            dfs(f[i]);
-        }   
+    for (int i = 0; i < n; i++) {
+        if (!vis[i]) {
+            int idx = i;
+            while (f[idx] != i) {
+                vis[idx] = 1;
+                idx = f[idx];
+            }
+            vis[idx] = 1;
+            cnt++;
+        }
+    }
 
-    cout << a;
+    cout << cnt << '\n';
 }
